06dbe7043ec6dd2bf03e04b86c141f02a7d75b46
MaxBocharnikov <bocharnikov3009@gmail.com>
2019-03-31 00:14:26 +0700

    Отрисовал карточку первого объявления. Задача выполнена

--- js/map.js
+++ js/map.js
@@ -12,9 +12,9 @@ var ADDRES_Y_INT_MAX = 630;
 
 
 //  Функция генерации целого числа
-var generateNumber = function (min , max) {
+var generateNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 //  Функция генерирует уникальный аватар
 var generateAvatar = function (generatedAdverts) {
@@ -208,9 +208,82 @@ var countRealLocation = function (pin) {
   return pin;
 };
 
+//  Функция отрисовывает фото
+var renderPhotosList = function (advert, photos) {
+  while (photos.firstChild) {
+    photos.removeChild(photos.firstChild);
+  }
+  for (var j = 0; j < advert.offer.photos.length; j++) {
+    var photo = document.createElement('img');
+    photo.src = advert.offer.photos[j];
+    photo.classList.add('popup__photo');
+    photo.style.width = '45px';
+    photo.style.height = '40px';
+    photo.alt = 'Фотография жилья';
+    photos.appendChild(photo);
+  }
+  return photos;
+};
+
+//  Функция отрисовывает фичи
+var renderFeatures = function (advert, features) {
+  while (features.firstChild) {
+    features.removeChild(features.firstChild);
+  }
+  for (var i = 0; i < advert.offer.features.length; i++) {
+    var feature = document.createElement('li');
+    feature.classList.add('popup__feature');
+    feature.classList.add('popup__feature--' + advert.offer.features[i]);
+    features.appendChild(feature);
+  }
+  return features;
+};
+//  Функция отрисовывает подробное объявление
+var rendeAdvertCard = function (advert, template) {
+  var mapCard = template.cloneNode(true);
+  mapCard.querySelector('.popup__title').textContent = advert.offer.title;
+  mapCard.querySelector('.popup__text--address').textContent = advert.offer.address;
+  mapCard.querySelector('.popup__text--price').textContent = advert.offer.price + '₽/ночь';
+  var type;
+  switch (advert.offer.type) {
+    case 'flat':
+      type = 'Квартира';
+      break;
+
+    case 'bungalo':
+      type = 'Бунгало';
+      break;
+
+    case 'house':
+      type = 'Дом';
+      break;
+
+    case 'palace':
+      type = 'Дворец';
+      break;
+  }
+  mapCard.querySelector('.popup__type').textContent = type;
+
+  mapCard.querySelector('.popup__text--capacity').textContent = advert.offer.rooms + ' комнаты для ' + advert.offer.guests + ' гостей';
+  mapCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
+
+
+  var features = mapCard.querySelector('.popup__features');
+  renderFeatures(advert, features);
+
+  mapCard.querySelector('.popup__description').textContent = advert.offer.description;
+
+  var photos = mapCard.querySelector('.popup__photos');
+  renderPhotosList(advert, photos);
+
+  mapCard.querySelector('.popup__avatar').src = advert.author.avatar;
+
+  return mapCard;
+};
+
+
 //  Step 1 - генерируем массив с объяектами-объявлениями
 var adverts = generateRandomAdverts(8);
-console.log(adverts);
 
 // Step 2 - у блока .map убираем класс map-faded
 var map = document.querySelector('.map');
@@ -225,7 +298,6 @@ for (var i = 0; i < adverts.length; i++) {
   var renderedPin = renderMark(adverts[i], pinTemplate);
   fragment.appendChild(renderedPin);
 }
-console.log(fragment);
 
 //  Step 4 - Добавляем отрисованные элементы в блок mapPins
 mapPins.appendChild(fragment);
@@ -235,4 +307,11 @@ for (var j = 0; j < pins.length; j++) {
   fragment.appendChild(countRealLocation(pins[i]));
 }
 mapPins.appendChild(fragment);
+
+
+// Step 5 - Отрисовываем объявление первой метки
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var filtersContainer = document.querySelector('.map__filters-container');
+map.insertBefore(rendeAdvertCard(adverts[0], cardTemplate), filtersContainer);
+
 
