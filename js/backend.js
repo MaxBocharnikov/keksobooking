417d0848ca7ddce7f6f800306df7070174b03fc2
MaxBocharnikov <bocharnikov3009@gmail.com>
2019-04-21 22:42:56 +0700

    Реализовал работу с сетью. Задание выполнено

--- js/backend.js
+++ js/backend.js
@@ -1,8 +1,8 @@
 'use strict';
 (function () {
 
-  window.load = function(onLoad, onError) {
-    var URL = 'https://js.dump.academy/keksobooking/data1';
+  window.load = function (onLoad, onError) {
+    var URL = 'https://js.dump.academy/keksobooking/data';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = 10000;
@@ -25,4 +25,32 @@
 
     xhr.send();
   };
+
+  window.save = function (data, onLoad, onError) {
+    var URL = 'https://js.dump.academy/keksobooking';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+    xhr.open('POST', URL);
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad();
+      } else {
+        onError('Значения некорректны. Попробуйте еще раз');
+      }
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Время ожидания истекло');
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Проблемы с сетью');
+    });
+
+    xhr.send(data);
+  };
+
+
 })();
